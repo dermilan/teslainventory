782f4a4044b9cb35a3c2e9fb08cb1b795a3c672d
milan <milan.jaksic@icloud.com>
2024-02-12 20:03:28 +0100

    button actions

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -4,7 +4,7 @@ import { NEXT_PUBLIC_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
-  let text: string | undefined = '';
+  let buttonPressed: any | undefined = '';
 
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
@@ -13,115 +13,46 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
+  buttonPressed = message?.button;
+  console.log('button pressed is: ' + buttonPressed);
+
   if (message?.button === 1) {
 
     console.log("button 1 pressed");
     
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: `Back`,
-          },
-        ],
-        image: {
-          src: `${NEXT_PUBLIC_URL}/park-1.png`,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-
-    // return NextResponse.redirect(
-    //   'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-    //   { status: 302 },
-    // );
   }
 
   if (message?.button === 2) {
 
     console.log("button 2 pressed");
     
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: `Back`,
-          },
-        ],
-        image: {
-          src: `${NEXT_PUBLIC_URL}/park-1.png`,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-
-    // return NextResponse.redirect(
-    //   'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-    //   { status: 302 },
-    // );
   }
 
   if (message?.button === 3) {
 
     console.log("button 3 pressed");
     
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: `Back`,
-          },
-        ],
-        image: {
-          src: `${NEXT_PUBLIC_URL}/park-1.png`,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-
-    // return NextResponse.redirect(
-    //   'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-    //   { status: 302 },
-    // );
   }
 
   if (message?.button === 4) {
 
     console.log("button 4 pressed");
     
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: `Back`,
-          },
-        ],
-        image: {
-          src: `${NEXT_PUBLIC_URL}/park-1.png`,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-
-    // return NextResponse.redirect(
-    //   'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-    //   { status: 302 },
-    // );
   }
 
-  // return new NextResponse(
-  //   getFrameHtmlResponse({
-  //     buttons: [
-  //       {
-  //         label: `Story: ${text} 🌲`,
-  //       },
-  //     ],
-  //     image: {
-  //       src: `${NEXT_PUBLIC_URL}/park-1.png`,
-  //     },
-  //     postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-  //   }),
-  // );
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: `Go Back`,
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/modely.png`,
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+  );
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
