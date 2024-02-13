978256c7c449e482f074d8effcd28a2a2698a819
milan <milan.jaksic@icloud.com>
2024-02-13 15:01:29 +0100

    addin groute for image gen

---
+++ app/api/og/route.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og';
+import { NEXT_PUBLIC_URL } from './config';
+
+
+// App router includes @vercel/og.
+// No need to install it.
+ 
+export const runtime = 'edge';
+ 
+export async function GET({data_my}) {
+  return new ImageResponse(
+    (
+      
+      <div
+        
+      style={{
+        fontSize: 60,
+        fontWeight: 700,
+        letterSpacing: -2,
+        backgroundImage: `url('${NEXT_PUBLIC_URL}/home-min.png')`,
+        color: 'yellow',
+        width: '100%',
+        height: '100%',
+        padding: '50px 200px',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        }}
+      >
+Model Y: Currently ${data_my.total_matches_found} available in the US. 
+      </div>
+    ),
+    {
+      width: 1440,
+      height: 900,
+    },
+  );
+}
+
