6633b51aa5b21376d5b6e34894c6758222bb9821
dermilan <157949312+dermilan@users.noreply.github.com>
2024-02-15 09:02:44 +0100

    image ref bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/home.png`,
+    src: `${NEXT_PUBLIC_URL}/home-min.png`,
     aspectRatio: '1:1',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
