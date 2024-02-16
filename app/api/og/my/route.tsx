643475c959261cdee197faa9073eb08415df0421
milan <milan.jaksic@icloud.com>
2024-02-16 21:01:17 +0100

    update all models

--- app/api/og/my/route.tsx
+++ app/api/og/my/route.tsx
@@ -2,15 +2,27 @@ import { ImageResponse } from "@vercel/og";
 import { NEXT_PUBLIC_URL } from '../../../config';
 import { kv } from '@vercel/kv';
 
-
 // App router includes @vercel/og.
 // No need to install it.
  
 export const runtime = 'edge';
- 
+
 export async function GET() {
 
+
   let data = await kv.get('my');
+  let lastFetchTimestamp:number | null = await kv.get('lastUpdate');
+  let timeStamp = '';
+
+  if (lastFetchTimestamp !== null) {
+    timeStamp = new Date(lastFetchTimestamp).toLocaleString();
+    console.log(timeStamp);
+  } else {
+    console.log("Unable to fetch last update timestamp.");
+  }
+
+
+
   const str: string = `${data}`;
 
   const fontData = await fetch(
@@ -20,23 +32,49 @@ export async function GET() {
   return new ImageResponse(
     (
       
+
+      <div
+      style={{
+        display: 'flex',
+        backgroundImage: `url('${NEXT_PUBLIC_URL}/modely-min.png')`,
+        width: '100%',
+        height: '100%',
+        padding: '50px 200px',
+      }}
+      >
+
       <div
         
       style={{
         fontSize: 60,
         fontWeight: 200,
         letterSpacing: -2,
-        backgroundImage: `url('${NEXT_PUBLIC_URL}/home-min.png')`,
         color: 'yellow',
-        width: '100%',
-        height: '100%',
-        padding: '50px 200px',
         textAlign: 'center',
         justifyContent: 'center',
         alignItems: 'flex-end',
         }}
       >
       {`Model Y: Currently ${str} available in the US.`}
+      </div>
+
+      <div
+            style={{
+              fontSize: 20,
+              fontWeight: 200,
+              letterSpacing: -2,
+              color: 'yellow',
+              textAlign: 'right',
+              position: 'absolute', // Add position absolute
+              top: 10, // Position at the bottom
+              right: 280, // Position at the right
+
+              }}
+      >
+      {`Data fetched: ${timeStamp}`}
+      </div>
+
+
       </div>
     ),
     {
